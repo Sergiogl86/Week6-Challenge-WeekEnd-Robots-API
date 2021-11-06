01c4e6d31a44ad15aa8c4bcc3c4b4f38f882a12b
Sergio Garcia <sergio.gl86@gmail.com>
2021-11-06 09:25:17 +0100

    Se han creado los test para el controller getIdRobot

--- server/controller/robotsController.test.js
+++ server/controller/robotsController.test.js
@@ -1,5 +1,5 @@
 const Robot = require("../../database/models/robots");
-const { getRobots } = require("./robotsController");
+const { getRobots, getIdRobot } = require("./robotsController");
 
 jest.mock("../../database/models/robots");
 
@@ -63,6 +63,60 @@ describe("Given a getRobots controller", () => {
       const next = jest.fn();
 
       await getRobots(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(error);
+      expect(error).toHaveProperty("code");
+      expect(error.code).toBe(400);
+    });
+  });
+});
+
+describe("Given a getIdRobot controller", () => {
+  describe("When it receives req with idRobot", () => {
+    test("Then it should call Robot.findOne with object _id: idRobot", async () => {
+      const robot = {
+        caracteristicas: {
+          velocidad: 100,
+          resistencia: 70,
+          FechaCeCreacion: "12-05-2018",
+        },
+        _id: "61857589666bcb02723c195c",
+        nombre: "WALL·E",
+        imagenUrl:
+          "https://iresiduo.com/sites/default/files/images/08-Wall-E.jpg",
+      };
+
+      const res = { json: jest.fn() };
+
+      const req = {
+        params: {
+          idRobot: 3,
+        },
+      };
+
+      Robot.findOne = jest.fn().mockResolvedValue(robot);
+
+      await getIdRobot(req, res, null);
+
+      expect(Robot.findOne).toHaveBeenCalledWith({ _id: 3 });
+      expect(res.json).toHaveBeenCalledWith(robot);
+    });
+  });
+  describe("When getIdRobot rejects, res", () => {
+    test("Then it should call the method json with a error rejected", async () => {
+      const error = {};
+
+      Robot.findOne = jest.fn().mockRejectedValue(error);
+
+      const req = {
+        params: {
+          idRobot: 3,
+        },
+      };
+      const res = {};
+      const next = jest.fn();
+
+      await getIdRobot(req, res, next);
 
       expect(next).toHaveBeenCalledWith(error);
       expect(error).toHaveProperty("code");
